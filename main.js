008d61ff321271571d2c6f82abf2ba94d71856bb
Abhinav <rawat.abhinav.2017446@gmail.com>
2023-04-27 01:17:08 +0530

    Added attendance System origin

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors({
     origin: `http://localhost:${PORT}`,
     origin: 'http://localhost:3000',
-    origin: `https://illustrious-youtiao-258f07.netlify.app`
+    origin: `https://attendancesystem.co.in`
 }))
 
 app.use('/api', router)
